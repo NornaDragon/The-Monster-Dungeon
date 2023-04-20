31d386fb6654ef5e06f18b46dd594ce4ddd1daa4
NornaDragon <126823407+NornaDragon@users.noreply.github.com>
2023-04-20 15:02:39 -0600

    initial commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+// The Monster Dungeon
+// Maria
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(mouseX - 25, mouseY - 25, 50, 50);
+}
